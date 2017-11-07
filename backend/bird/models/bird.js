80483bebe7edcddaf41b8619eb14eca2c3e241b8
lairdjl@rose-hulman.edu <laird.jacob@gmail.com>
2017-11-07 12:41:55 -0500

    got backend connection

---
+++ backend/bird/models/bird.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var birdSchema = new Schema({
+    name: {type: String, required: true},
+    url: String,
+    date_added: Date,
+    upvotes: Number,
+    downvotes: Number,
+    // views:
+    bio: String,
+    score: Number
+    // user_key: 
+   
+});
+
+module.exports = {model: mongoose.model('Bird', birdSchema), schema: birdSchema};
--- backend/bird/server.js
+++ backend/bird/server.js
@@ -1,11 +1,47 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+const port = 3000;//8080
+
+require('./models/bird.js');
+// require('./models/bird_metrics.js');
+// require('./models/boards.js');
+// require('./models/user.js');
+
+//instantiates all models as objects
+const Bird = mongoose.model('Bird');
+// const Bird_Metrics = mongoose.model('Bird_Metrics');
+// const Boards = mongoose.model('Boards');
+// const User = mongoose.model('User');
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'public')));
 
+// const router = express.Router();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, Methods, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/bird', ((req, res) => {
+    Bird.find({}, (err, bird) => {
+        if (err) {
+            res.status(404);
+            handleError(err, res, 'Not Found');
+        } else {
+            res.status(200).json(bird);
+        }
+    });
+}));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
@@ -23,4 +59,9 @@ app.get('/law', function(req, res) {
 
 });
 
-app.listen(8080);
+
+mongoose.connect('mongodb://localhost:27017/birddb');
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}...`);
+});
